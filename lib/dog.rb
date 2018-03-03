303c4e3c97ea6f9cf97e4ada1463020c7606486d
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-03 03:12:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,10 @@
 class Dog
   def initialize(dogs_name = breed)
     @name = dogs_name
-  else
-    @breed = breed
+    @breed = "Mutt"
   end
   def name
     @name
-  end
-  def breed
-    @breed
-    puts "Mutt"
   end
 
 end
