0c15e038e73e7681b697f0a4145ab5b8ac93f4f2
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-03 03:21:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,9 @@
 class Dog
+  attr_accessor :name, :breed 
   def initialize(dogs_name, breed = "Mutt")
     @name = dogs_name
     @breed = breed
   end
-  def name
-    @name
-    @breed 
-  end
+  
 
 end
